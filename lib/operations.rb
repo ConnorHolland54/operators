c9fa234f4faab8d87486cc7b9a7f5ff08bd1fe93
Connor Holland <supersoccergoalie123@gmail.com>
2020-11-12 11:34:44 -0700

    commit tree 64c2aa547be69e4fa46a95af8760109f1ee8cc58

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,20 @@
 def unsafe?(speed)
-
+    if speed > 60 
+        return true
+    elsif speed < 40
+        return true
+    elsif speed > 40 && speed < 60
+        return false
+    end
 end
 
 
 
 def not_safe?(speed)
-	
+    # result = true
+    # speed > 60 || speed < 40 ? result = true : result = false
+    # return result
+    return speed > 60 || speed < 40 ? true : false
 end
 	
 
